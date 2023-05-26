a296ae64e4090aba87662032dc4c13bfbbb3b646
Allex Aldir Campos de Assis <allex.assis@grupofm.local>
2023-05-26 11:36:15 -0300

    Add => Aulas
    Estudos ao getters e setters

---
+++ aulas/Getter_Setters.ts
@@ -0,0 +1,41 @@
+//Metodo antigo
+
+export class Pessoa {
+    constructor(
+        private Nome: string,
+        private Idade: number,
+        private CPF: string
+    ) { }
+
+    getNome(): void {
+        console.log(this.Nome)
+    }
+
+    setNome(x: string): void {
+        this.Nome = x
+    }
+}
+
+
+//Metodo autal
+
+export class Aluno {
+    constructor(
+        private Nome: string,
+        private Idade: number,
+        private _CPF: string
+    ) { }
+
+
+    get CPF(): string {
+        return this._CPF
+    }
+
+    set CPF(x: string) {
+        this._CPF = x;
+    }
+}
+
+const AL1 = new Aluno('Lucas', 25, '2345678')
+AL1.CPF = '6767677676'
+console.log(AL1.CPF)
